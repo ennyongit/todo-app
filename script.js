85dd34aadaad2304e6abfc56c84731c2303dfa1d
Eni Bondarenko <bondarenkoenny@gmail.com>
2025-02-07 18:48:31 +0100

    Added delete btn functionality

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let taskInput = document.getElementById("task-input");
 let addTaskBtn = document.getElementById("add-task-btn");
 let taskList = document.getElementById("task-list");
 
+let taskId = 0;
+
 function addTask() {
     let taskText = taskInput.value.trim();
 
@@ -10,11 +12,19 @@ function addTask() {
         return;
     } 
 
+    taskId++;
+
     let li = document.createElement("li");
+    li.id = "task" + taskId; 
     li.innerHTML = `
      <span>${taskText}</span>
      <button class="delete-btn">Delete</button>
   `
+  let deleteBtn = li.querySelector(".delete-btn");
+  deleteBtn.addEventListener("click", function (){
+    let taskToDelete = document.getElementById(li.id);
+    taskToDelete.remove();
+  })
     taskList.appendChild(li);
     taskInput.value = "";
 }
